76e7599ff0421ed9bfc5e71b3a36a503c0227614
molly <mollynzembi@gmail.com>
2024-07-10 21:55:32 +0300

    created homepage and order list

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,18 @@
-import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import NavBar from './NavBar'; // Corrected path to import NavBar correctly
+import OrderList from '../pages/OrderList'; // Corrected path to import OrderList correctly
 
-function App() {
-  return <h1>Project Client</h1>;
-}
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>My Restaurant Orders</h1>
+      <OrderList />
+    </div>
+  );
+};
 
 export default App;
+
+
+
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
+
