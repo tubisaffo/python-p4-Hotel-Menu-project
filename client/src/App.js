8d3b2e2128d2bb89445f4faeb4b2c07fe602f902
mosh9098 <mosh9098@gmail.com>
2024-07-10 15:40:04 +0300

    firstfront end commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';   // Adjusted path to LoginPage
+import MainPage from './pages/MainPage';     // Adjusted path to MainPage
+import OrdersPage from './pages/OrdersPage'; // Adjusted path to OrdersPage
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LoginPage} />
+        <Route path="/main" component={MainPage} />
+        <Route path="/orders" component={OrdersPage} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
