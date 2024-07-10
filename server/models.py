d8e01f21204d8105d182ac6cf29a6ae1b415429b
Gathuku Samwel <gathsam3446@gmail.com>
2024-07-10 15:16:07 +0300

    Creating models

--- server/models.py
+++ server/models.py
@@ -1,26 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
-db = SQLAlchemy()
+from config import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+
+    def _repr_(self):
+        return f'<User {self.username}>'
 
 class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    description = db.Column(db.String(200))
+    name = db.Column(db.String(120), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
     price = db.Column(db.Float, nullable=False)
-    created_by = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def _repr_(self):
+        return f'<MenuItem {self.name}>'
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    order_date = db.Column(db.DateTime, default=datetime.utcnow)
-
-class OrderItem(db.Model):
-    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), primary_key=True)
-    menu_item_id = db.Column(db.Integer, db.ForeignKey('menu_item.id'), primary_key=True)
+    menu_item_id = db.Column(db.Integer, db.ForeignKey('menu_item.id'), nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+
+    def _repr_(self):
+        return f'<Order {self.id}>'
